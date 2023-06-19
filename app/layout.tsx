115432ef33fe588762ef058ba5ac01074db266d7
ista1024 <ista1024@gmail.com>
2023-06-20 08:14:59 +1000

    init tailwind css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
 export default function RootLayout({
   children,
 }: {
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <h1 className="text-3xl font-bold underline">Hello World. </h1>
+        <Link href="/about">About</Link>
+      </div>
+    </main>
   )
 }
