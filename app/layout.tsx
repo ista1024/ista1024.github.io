af3b0ca57b085cd4a8f32ce4078cb9f846e624dd
ista1024 <ista1024@gmail.com>
2023-06-20 08:23:05 +1000

    add theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import './globals.css'
+import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +11,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
