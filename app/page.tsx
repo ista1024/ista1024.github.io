8dc4d4504d320ee13324fab3ab8638ce11dbf94f
ista1024 <ista1024@gmail.com>
2023-06-26 17:13:27 +1000

    hero page  - add text animations and styles    * source: https://alvarotrigo.com/blog/css-text-animations/    and used ChatGPT to implement

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,69 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import '@/styles/hero.css';
 
 export default function Home() {
+  const words = ["Positive", "Proactive", "Challenging"];
+
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [currentOffset, setCurrentOffset] = useState(0);
+  const [forwards, setForwards] = useState(true);
+
+  // delay for the backward to be processed
+  const [skipCount, setSkipCount] = useState(0);
+  const skipDelay = 15;
+  const speed = 70;
+
+  useEffect(() => {
+    const animateText = () => {
+      const currentWord = words[currentWordIndex];
+
+      if (forwards) {
+        if (currentOffset >= currentWord.length) {
+          setSkipCount(skipCount + 1);
+          if (skipCount === skipDelay) {
+            setForwards(false);
+            setSkipCount(0);
+          }
+        }
+      } else {
+        if (currentOffset === 0) {
+          setForwards(true);
+          setCurrentWordIndex((prevIndex) =>
+            prevIndex === words.length - 1 ? 0 : prevIndex + 1
+          );
+        }
+      }
+
+      if (skipCount === 0) {
+        forwards
+          ? setCurrentOffset((prevOffset) => prevOffset + 1)
+          : setCurrentOffset((prevOffset) => prevOffset - 1);
+      }
+    };
+
+    const interval = setInterval(animateText, speed);
+
+    return () => clearInterval(interval);
+  }, [currentWordIndex, currentOffset, forwards, skipCount]);
+
+
   return (
     <main>
       <div className="contents">
-        <h1 className="text-6xl font-bold">Welcome!</h1>
-        <p className='mt-2'>I'm #Positive #Proactive #Challenging developer.</p>
+        <div className="flex flex-col justify-center items-center m-2">
+          <h1 className="text-6xl font-bold">Welcome!</h1>
+          <p className='text-l mt-4'>
+            I'm a
+          </p>
+          <h6 className="hash-text text-xl font-bold my-2">
+            #{words[currentWordIndex].substring(0, currentOffset)}
+          </h6>
+          <p className='text-l'>
+            JavaScript Developer!
+          </p>
+        </div>
       </div>
     </main>
   )
