f7caf7c0d4a278d99cf19f3262bce411fc1503a1
ista1024 <ista1024@gmail.com>
2023-07-01 20:29:41 +1000

    Deploy

---
+++ _next/static/chunks/app/todo/page-e38c848fd9c0e6c1.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[435],{5404:function(e,r,t){Promise.resolve().then(t.bind(t,3101))},3101:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return s}});var n=t(9268);function s(){return(0,n.jsx)("main",{children:(0,n.jsx)("div",{className:"contents",children:(0,n.jsxs)("div",{className:"flex flex-col justify-center items-center m-2",children:[(0,n.jsx)("h1",{className:"head_text",children:"Todo!"}),(0,n.jsx)("p",{className:"text-l mt-4",children:"I'm a"}),(0,n.jsx)("p",{className:"text-l",children:"JavaScript Developer!"})]})})})}},3177:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(6006),s=Symbol.for("react.element"),c=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),o=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function l(e,r,t){var n,l={},a=null,f=null;for(n in void 0!==t&&(a=""+t),void 0!==r.key&&(a=""+r.key),void 0!==r.ref&&(f=r.ref),r)c.call(r,n)&&!i.hasOwnProperty(n)&&(l[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===l[n]&&(l[n]=r[n]);return{$$typeof:s,type:e,key:a,ref:f,props:l,_owner:o.current}}r.jsx=l,r.jsxs=l},9268:function(e,r,t){"use strict";e.exports=t(3177)}},function(e){e.O(0,[253,769,744],function(){return e(e.s=5404)}),_N_E=e.O()}]);
