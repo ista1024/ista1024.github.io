9a29145623fe12b4f33f22a4dc11efbe727ee636
ista1024 <ista1024@gmail.com>
2026-03-04 12:43:31 +1100

    separate letter animation from main landing page to component

---
+++ components/letterAnimation.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const words = ["Positive", "Teamworking", "Challenging"];
+
+export default function LetterAnimation() {
+
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [currentOffset, setCurrentOffset] = useState(0);
+  const [forwards, setForwards] = useState(true);
+
+  // delay for the backward to be processed
+  const [skipCount, setSkipCount] = useState(0);
+  const skipDelay = 15;
+  const speed = 70;
+
+  useEffect(() => {
+    const animateText = () => {
+      const currentWord = words[currentWordIndex];
+
+      if (forwards) {
+        if (currentOffset >= currentWord.length) {
+          setSkipCount(skipCount + 1);
+          if (skipCount === skipDelay) {
+            setForwards(false);
+            setSkipCount(0);
+          }
+        }
+      } else {
+        if (currentOffset === 0) {
+          setForwards(true);
+          setCurrentWordIndex((prevIndex) =>
+            prevIndex === words.length - 1 ? 0 : prevIndex + 1
+          );
+        }
+      }
+
+      if (skipCount === 0) {
+        forwards
+          ? setCurrentOffset((prevOffset) => prevOffset + 1)
+          : setCurrentOffset((prevOffset) => prevOffset - 1);
+      }
+    };
+
+    const interval = setInterval(animateText, speed);
+
+    return () => clearInterval(interval);
+  }, [currentWordIndex, currentOffset, forwards, skipCount]);
+
+  return (
+    <h6 className="hash-text text-xl font-bold my-2">
+      #{words[currentWordIndex].substring(0, currentOffset)}
+    </h6>
+  );
+}
